cacd0a3431b615af30e6fa93eead1294a8b0644b
Amy <70098892+Shtwick@users.noreply.github.com>
2026-05-12 01:43:34 +0200

    Added basic menu
    To work on later : Lobby Map

---
+++ Assets/Scenes/Dev/Amy/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public AudioSource musicSource;
+
+    // We set up the coordinates for where the menu should go
+    private Vector3 offScreenPosition = new Vector3(0, 2000, 0); // 2000 pixels UP
+    private Vector3 onScreenPosition = new Vector3(0, 0, 0);     // Dead center
+
+    void Start()
+    {
+        if (optionsPanel != null)
+        {
+            // Teleport off-screen on startup!
+            optionsPanel.transform.localPosition = offScreenPosition;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene 1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit button was clicked! Exiting game...");
+        Application.Quit();
+    }
+
+    public void OpenOptions()
+    {
+        // Bring it back to the center of the screen
+        optionsPanel.transform.localPosition = onScreenPosition;
+    }
+
+    public void CloseOptions()
+    {
+        // Throw it back off-screen
+        optionsPanel.transform.localPosition = offScreenPosition;
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        if (musicSource != null)
+        {
+            musicSource.volume = volume;
+        }
+    }
+}
