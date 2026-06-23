035011844356a85a71535b266d6d36dd69a01f83
PandEwok <diegojaillot@outlook.fr>
2026-06-23 12:51:58 +0200

    boss AI start

--- Assets/Entities/Enemies/Boss/Alchemist/Script/Alchemist_AI.cs
+++ Assets/Entities/Enemies/Boss/Alchemist/Script/Alchemist_AI.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
+using System.Threading.Tasks;
 
 public class Alchemist_AI : Boss_AI
 {
-    
+    public async override Task playTurn(GameObject target)
+    {
+        if (!stats.isDizzy)
+        {
+            await closeAttack(target);
+        }
+        await base.playTurn(target);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
 }
