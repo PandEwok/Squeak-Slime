a1350ee8504d3188c1f8ffd46d28b0ba80ab190c
Romainchvd <rominou.dumont@gmail.com>
2026-06-25 09:12:25 +0200

    Retours Anaïs
    - Ajout indicateur tab
    - Ajout indicateur click

--- Assets/UI/UIManager/UiManager.cs
+++ Assets/UI/UIManager/UiManager.cs
@@ -8,6 +8,9 @@ public class UiManager : MonoBehaviour
     [SerializeField] private GameObject qteWarning;
     [SerializeField] private GameObject gradeDisplay;
     public GameObject spamIndicator;
+    [SerializeField] private GameObject leftClickIndicator;
+    [SerializeField] private Vector3 leftClickIndicatorPosition = new Vector3(8, 4, 1);
+    private GameObject leftClickIndicatorInstance;
     [SerializeField] private Color waitColor = new Color(1f, 0, 0, 1f);
     [SerializeField] private Color readyColor = new Color(0, 1f, 0, 1f);
     private SpriteRenderer qteSprite;
@@ -25,25 +28,32 @@ public class UiManager : MonoBehaviour
             spamIndicator.SetActive(false);
         }
     }
-    public void ShowQTE(bool mustDisplay, bool isReady = false)
+    public void ShowQTE(bool mustDisplay, bool isReady = false, bool isFireBall = false)
     {
         if (mustDisplay)
         {
             qteWarning.SetActive(true);
-            if(isReady)
+            if (isReady)
             {
                 qteSprite.color = readyColor;
                 AudioManager.Instance.PlaySFX("QTE");
+
             }
             else
             {
                 qteSprite.color = waitColor;
             }
+            if (!isFireBall)
+            {
+                DoShowLeftClickIndicator(true);
+            }
         }
+
         else
         {
             qteSprite.color = waitColor;
             qteWarning.SetActive(false);
+            DoShowLeftClickIndicator(false);
         }
     }
     public void DisplayGrade(GradeScript.Grade grade, bool display)
@@ -53,6 +63,27 @@ public class UiManager : MonoBehaviour
             gradeScript.StopAllCoroutines();
             gradeScript.gameObject.SetActive(true);
             StartCoroutine(gradeScript.GradeDisplay(grade, display));
+        }
+    }
+    public void DoShowLeftClickIndicator(bool show)
+    {
+        if(leftClickIndicatorInstance == null)
+        {
+            leftClickIndicatorInstance = Instantiate(leftClickIndicator, leftClickIndicatorPosition, Quaternion.identity);
+        }
+        if (show)
+        {
+            if(leftClickIndicatorInstance != null)
+            {
+                leftClickIndicatorInstance.SetActive(true);
+            }
+        }
+        else
+        {
+            if(leftClickIndicatorInstance != null)
+            {
+                leftClickIndicatorInstance.SetActive(false);
+            }
         }
     }
 }
