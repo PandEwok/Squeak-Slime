c4e0f7877ecb1df436a1992528bb2af10cd14f89
Romainchvd <rominou.dumont@gmail.com>
2026-06-27 17:54:55 +0200

    Ajout scène de fin

---
+++ Assets/UI/Credits/CreditsScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CreditsScript : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDocument;
+    private VisualElement root;
+    private Button closeButton;
+
+
+    private void Awake()
+    {
+        if (uiDocument == null)
+        {
+            uiDocument = GetComponent<UIDocument>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (uiDocument == null) return;
+
+        root = uiDocument.rootVisualElement;
+        if (root != null)
+        {
+            closeButton = root.Q<Button>("Close");
+
+            if (closeButton != null)
+            {
+                closeButton.clicked += QuitGame;
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Closing game...");
+
+        //Debug
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        //App
+#else
+            Application.Quit();
+#endif
+    }
+}
